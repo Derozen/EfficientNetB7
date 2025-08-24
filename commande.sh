65d00d7e361ce0078bb577a17c15e0168848d600
Derozen <manbationo@gmail.com>
2025-08-24 09:26:59 +0100

    commit

---
+++ commande.sh
@@ -0,0 +1,6 @@
+!unzip batch1.zip -d data/train/images
+!unzip batch1_mask.zip -d data/train/masks
+!unzip batch2.zip -d data/train/images
+!unzip batch2_mask.zip -d data/train/masks
+!unzip valid.zip -d data/valid/images
+!unzip valid_mask.zip -d data/valid/masks
